5921942707178fc983696cf46095932ba3d0b3e9
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2020-07-20 14:17:17 +0200

    Add Kong main file

---
+++ kong.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""
+Kong: Koncurrent Places Squasher
+
+Input file format: .net
+
+This file is part of Kong.
+
+Kong is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Kong is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Kong. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+__author__ = "Nicolas AMAT, LAAS-CNRS"
+__contact__ = "namat@laas.fr"
+__license__ = "GPLv3"
+__version__ = "1.0.0"
+
+from pt import PetriNet
+from e_abstraction import System, Relation
