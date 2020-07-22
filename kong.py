dca88c8925546de43f963c2ebcc8d1d5b570f0c6
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2020-07-22 11:29:56 +0200

    Main file

--- kong.py
+++ kong.py
@@ -26,20 +26,23 @@ __contact__ = "namat@laas.fr"
 __license__ = "GPLv3"
 __version__ = "1.0.0"
 
+from concurrency_matrix import ConcurrencyMatrix
 from pt import PetriNet
 from e_abstraction import System, Relation
 
 import argparse
 import logging as log
+import os
 import subprocess
+import sys
 import tempfile
 
 
 def main():
     """ Main Function.
     """
+    # Arguments parser
     parser = argparse.ArgumentParser(description='Koncurrent Places Squasher')
-
     parser.add_argument('--version',
                         action='version',
                         version='%(prog)s 1.0.0',
@@ -60,15 +63,48 @@ def main():
                         type=int,
                         default=60,
                         help='a limit on execution time')
-
     results = parser.parse_args()
 
+    # Verbose option
     if results.verbose:
         log.basicConfig(format="%(message)s", level=log.DEBUG)
+        stdout = None
     else:
         log.basicConfig(format="%(message)s")
+        stdout = subprocess.DEVNULL
 
-   
+    # Read input net
+    initial_net = PetriNet(results.infile)
+
+    # Reduce input net
+    f_reduced_net = tempfile.NamedTemporaryFile(suffix='.net')
+    subprocess.run(["reduce", "-rg,redundant,compact,convert,transitions", "-PNML", results.infile, f_reduced_net.name])
+
+    # Convert reduced net to .pnml format
+    f_reduced_pnml = tempfile.NamedTemporaryFile(suffix='.pnml')
+    subprocess.run(["ndrio", f_reduced_net.name, f_reduced_pnml.name])
+
+    # Read reduced net
+    reduced_net = PetriNet(f_reduced_pnml)
+
+    # Read system of equations linking both nets
+    system = System(f_reduced_net.name, initial_net.places, reduced_net.places) 
+
+    # Convert reduced net to .nupn format
+    PNML2NUPN = os.getenv('PNML2NUPN')
+    if not PNML2NUPN:
+        f_reduced_net.close()
+        f_reduced_pnml.close()
+        sys.exit("Environment variable PNML2NUPN not defined!")
+    subprocess.run(["java", "-jar", PNML2NUPN, f_reduced_pnml.name], stdout=stdout)
+
+    # Compute concurrency matrix of the reduced net
+    matrix_reduced = subprocess.run(["caesar.bdd", "-concurrent-places", f_reduced_pnml.name.replace('.pnml', '.nupn')], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    concurrency_matrix = ConcurrencyMatrix(initial_net, reduced_net, system, matrix_reduced)
+
+    # Close temporary files
+    f_reduced_net.close()
+    f_reduced_pnml.close()
 
 if __name__ == '__main__':
     main()
