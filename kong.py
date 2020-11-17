c621d41b2ef9371e539854e451d72697563a89fc
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2020-11-17 16:24:45 +0100

    Add options: save/read reduced net

--- kong.py
+++ kong.py
@@ -69,6 +69,18 @@ def main():
                         type=str,
                         help="input Petri net (.pnml format)")
 
+    group_reductions = parser.add_mutually_exclusive_group()
+
+    group_reductions.add_argument('--save-reduced', '-sr',
+                                    action='store_true',
+                                    help="save the reduced net")
+
+    group_reductions.add_argument('--reduced', '-r',
+                                    action='store',
+                                    dest='reduced_net',
+                                    type=str,
+                                    help='reduced Petri Net (.net format)')
+
     parser.add_argument('-pl', '--place_names',
                         action='store_true',
                         help="display place names")
@@ -95,19 +107,26 @@ def main():
     log.info("> Read the input Petri net")
     initial_net = PetriNet(results.infile)
 
-    # Reduce input net
-    log.info("> Reduce the input Petri net")
-    start_time = time.time()
-    f_reduced_net = tempfile.NamedTemporaryFile(suffix='.net')
-    subprocess.run(["reduce", "-rg,redundant,compact+,mg,4ti2", "-redundant-limit", "650", "-redundant-time", "10", "-inv-limit", "1000", "-inv-time", "10", "-PNML", results.infile, f_reduced_net.name])
-    if results.time:
-        print("# Reduction time:", time.time() - start_time)
-    transition_renamer(f_reduced_net.name)
+    # Manage reduced net
+    if results.reduced_net:
+        reduced_net_filename = results.reduced_net
+    else:
+        log.info("> Reduce the input Petri net")
+        if results.save_reduced:
+            reduced_net_filename = results.infile.replace('.pnml', '_reduced.net')
+        else:
+            f_reduced_net = tempfile.NamedTemporaryFile(suffix='.net')
+            reduced_net_filename = f_reduced_net.name
+        start_time = time.time()
+        subprocess.run(["reduce", "-rg,redundant,compact+,mg,4ti2", "-redundant-limit", "650", "-redundant-time", "10", "-inv-limit", "1000", "-inv-time", "10", "-PNML", results.infile, reduced_net_filename])
+        if results.time:
+            print("# Reduction time:", time.time() - start_time)
+    transition_renamer(reduced_net_filename)
 
     # Convert reduced net to .pnml format
     log.info("> Convert the reduced Petri net to '.pnml' format")
     f_reduced_pnml = tempfile.NamedTemporaryFile(suffix='.pnml')
-    subprocess.run(["ndrio", f_reduced_net.name, f_reduced_pnml.name])
+    subprocess.run(["ndrio", reduced_net_filename, f_reduced_pnml.name])
 
     # Read reduced net
     log.info("> Read the reduced Petri net")
@@ -117,7 +136,8 @@ def main():
     if results.reduction_ratio:
         print("# Reduction ratio:", (1 - reduced_net.number_places / initial_net.number_places) * 100)
         # Close temporary files
-        f_reduced_net.close()
+        if not (results.save_reduced or results.reduced_net):
+            f_reduced_net.close()
         f_reduced_pnml.close()
         return
 
@@ -128,7 +148,8 @@ def main():
         log.info("> Convert the reduced Petri net to '.nupn' format")
         PNML2NUPN = os.getenv('PNML2NUPN')
         if not PNML2NUPN:
-            f_reduced_net.close()
+            if not (results.save_reduced or results.reduced_net):
+                f_reduced_net.close()
             f_reduced_pnml.close()
             sys.exit("Environment variable PNML2NUPN not defined!")
         subprocess.run(["java", "-jar", PNML2NUPN, f_reduced_pnml.name], stdout=stdout)
@@ -143,14 +164,15 @@ def main():
 
     # Compute the concurrency matrix of the initial net using the system of equations and the concurrency matrix from the reduced net
     log.info("> Change of basis")
-    concurrency_matrix = ConcurrencyMatrix(initial_net, reduced_net, f_reduced_net.name, matrix_reduced, results.place_names)
+    concurrency_matrix = ConcurrencyMatrix(initial_net, reduced_net, reduced_net_filename, matrix_reduced, results.place_names)
 
     # Show computation time
     if results.time:
         print("# Computation time:", time.time() - start_time)
 
     # Close temporary files
-    f_reduced_net.close()
+    if not (results.save_reduced or results.reduced_net):
+        f_reduced_net.close()
     f_reduced_pnml.close()
 
 if __name__ == '__main__':
