079e035729e95fd62aea51e70f68d0f4d6e54e6d
Nicolas Amat <nicolas.amat@grenoble-inp.org>
2020-12-07 22:04:40 +0100

    Update instances filter script

--- benchmark/instances_filter.py
+++ benchmark/instances_filter.py
@@ -34,16 +34,16 @@ def main():
 
     parser.add_argument('ratio',
                         metavar='ratio',
-                        type=int,
+                        type=float,
                         help='minimum reduction ratio')
 
     results = parser.parse_args()
 
-    # Read DataFrame
+    # Read data frame
     df_reductions = pd.read_csv(results.path_reductions_csv)
 
     # Read instances list
-    with open(results.path_insances_list, 'r') as fp_reductions:
+    with open(results.path_instances_list, 'r') as fp_reductions:
         instances = fp_reductions.read().splitlines()
 
     # Write the filtered instances list
@@ -54,7 +54,7 @@ def main():
             instance_name = os.path.basename(instance).replace('.pnml', '')
             # Get the corresponding reduction ratio
             ratio = df_reductions.query('INSTANCE == "{}"'.format(instance_name))['RATIO'].values
-            # Write the instance in the output list if the reduction ratio is greater than the minimum ratio
+            # Write the instance in the output list if the reduction ratio is greater than the minimum one
             if ratio and  ratio[0] >= results.ratio:
                 fp_filtered.write(instance + '\n')
                 print(instance_name)
