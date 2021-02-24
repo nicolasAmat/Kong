730d62f0dac8c25d4ef6cf0b5ac71339a0aed589
Nicolas Amat <namat@laas.fr>
2021-02-25 00:19:30 +0100

    New version of the algorithm

--- token_flow_graph.py
+++ token_flow_graph.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Token Flow Graph Method
+Token Flow Graph Module
 
 This file is part of Kong.
 
@@ -22,10 +22,11 @@ along with Kong. If not, see <https://www.gnu.org/licenses/>.
 __author__ = "Nicolas AMAT, LAAS-CNRS"
 __contact__ = "namat@laas.fr"
 __license__ = "GPLv3"
-__version__ = "1.0.0"
+__version__ = "2.0.0"
 
 import itertools
 import re
+from collections import deque
 
 from graphviz import Graph
 
@@ -45,7 +46,12 @@ class TFG:
         # Nodes initialization
         self.nodes = {}
         self.init_nodes()
-        self.marked_root = self.get_node('1')
+    
+        # Non-dead roots
+        self.non_dead_roots = []
+        self.counter_non_dead_roots = 0
+
+        # Dead root
         self.dead_root = self.get_node('0')
 
         # Parse the system of equations and build the Token Flow Graph
@@ -96,6 +102,14 @@ class TFG:
         """ Return the node that corresponds to the id if it exists,
             otherwise create a new one and return it.
         """
+        if id_node == '1':
+            self.counter_non_dead_roots += 1
+            id_node = "{}#{}".format(id_node, self.counter_non_dead_roots)
+            node = Node(id_node, additional=True)
+            self.nodes[id_node] = node
+            self.non_dead_roots.append(node)
+            return node
+
         if id_node in self.nodes:
             return self.nodes[id_node]
         else:
@@ -132,15 +146,24 @@ class TFG:
 
         # Redundance (Constant or Duplication or Shortcut)
         if kind == 'R':
-            removed_place = nodes.pop(0)
-            for node in nodes:
-                node.redundant.append(removed_place)
+            child = nodes.pop(0)
+            for parent in nodes:
+                parent.redundant.append(child)
+                child.parents.append(parent)
             return
 
         # Agglomeration
         if kind == 'A':
-            new_place = nodes.pop(0)
-            new_place.agglomerated += nodes
+            parent = nodes.pop(0)
+            for child in nodes:
+                parent.agglomerated.append(child)
+                child.parents.append(parent)
+            return
+
+        # MG (TODO: missing the full implementation for using the `mg` option in the Reduce tool)
+        if kind == 'I':
+            constant = nodes.pop()
+            constant.agglomerated += nodes
             return
 
         raise ValueError("Invalid reduction equation")
@@ -148,50 +171,100 @@ class TFG:
     def change_of_basis(self):
         """ Change of Basis method.
         """
-        # Propagate marked root
-        self.token_propagation(self.marked_root, '1', memoize=True)
-        # Partial relation case
+        # Propagate non-dead roots
+        for non_dead_root in self.non_dead_roots:
+            self.token_propagation(non_dead_root, '1', memoize=True)
+        
+        # Case: partial relation
         if not self.complete_matrix:
             # Propagate dead root
             self.token_propagation(self.dead_root, '0', memoize=True)
-            # Product with marked root
-            self.product(self.marked_root.children, self.dead_root.children, '0')
 
         # Propagate roots values (from the reduced net)
         for i in range(self.reduced_net.number_places):
-            node = self.get_node(self.reduced_net.places[i])
+
+            # Get corresponding root and matrix value
+            root = self.get_node(self.reduced_net.places[i])
             value = self.matrix_reduced[i][i]
 
             # Reachable root
             if value == '1':
-                self.token_propagation(node, value, memoize=True)
-                # Product with marked root
-                self.product(self.marked_root.children, node.children, value)
+                self.token_propagation(root, value, memoize=True)
+                # Product with non-dead roots
+                for non_dead_root in self.non_dead_roots:
+                    self.product(non_dead_root.successors, root.successors, value)
 
-            # Partial relation case
-            if not self.complete_matrix:
-                # Non necessary reachable root propagation
-                if value != '1':
-                    self.token_propagation(node, value, memoize=True)
-                # Product with dead root
-                self.product(self.dead_root.children, node.children, '0')
-                # Product with marked root
-                if value == '0':
-                    self.product(self.marked_root.children, node.children, '0')
+            # Case: partial relation and root not already propagated
+            if not self.complete_matrix and value != '1':
+                self.token_propagation(root, value, memoize=True)
+    
+        # Product with non-dead roots
+        for non_dead_root_1, non_dead_root_2 in itertools.combinations(self.non_dead_roots, 2):
+            self.product(non_dead_root_1.successors, non_dead_root_2.successors, '1')
 
         # Propagate the concurrency relation from the reduced matrix
         for i, line in enumerate(self.matrix_reduced):
+            
+            # Skip dead roots
+            if line[i] == '0':
+                continue
+            
+            # Only iterate over the lower triangle (symmetric relation)
             for j, concurrency in enumerate(line[:-1]):
-                # If two root are concurrent then each pair of children are concurrent
+
+                # The product of the concurrent roots' successors is included in the concurrency relation
                 if concurrency == '1':
-                    node1, node2 = self.get_node(self.reduced_net.places[i]), self.get_node(self.reduced_net.places[j])
-                    self.product(node1.children, node2.children, '1')
-                # Partial relation case
-                # (If two roots are independent then each pair of children are independent too,
-                # except for the intersection that is managed by the product method.)
+                    root_1, root_2 = self.get_node(self.reduced_net.places[i]), self.get_node(self.reduced_net.places[j])
+                    self.product(root_1.successors, root_2.successors, '1')
+
+                # Case: partial relation
                 if not self.complete_matrix and concurrency == '0':
-                    node1, node2 = self.get_node(self.reduced_net.places[i]), self.get_node(self.reduced_net.places[j])
-                    self.product(node1.children, node2.children, '0')
+                    # Set roots as independent
+                    root_1, root_2 = self.get_node(self.reduced_net.places[i]), self.get_node(self.reduced_net.places[j])
+                    root_1.independent.add(root_2)
+                    root_2.independent.add(root_1)
+
+        # Case: partial relation
+        if not self.complete_matrix:
+
+            # Queue initialization
+            queue = deque()
+            for place_id in self.reduced_net.places:
+                queue.append(self.get_node(place_id))
+
+            while queue:
+                # Get first node in the queue
+                node = queue.popleft()
+
+                # Iterate over the intersection of the independent places from the parents
+                if node.parents:
+                    for independent_node in set.intersection(*[parent.independent for parent in node.parents]): 
+                        # Add the independency relation in nodes
+                        node.independent.add(independent_node)
+                        independent_node.independent.add(node)
+
+                # Add the independency relation in the matrix for places from the initial net
+                if not node.additional:
+                    self.product([node], [independent_node for independent_node in node.independent if not independent_node.additional], '0' )
+
+                # Add children to the queue
+                for child in node.agglomerated + node.redundant:
+                    queue.append(child)
+
+        # Case: partial relation
+        # Dead places are independent to all others places
+        dead_columns = []
+        for i, row in enumerate(self.matrix_initial):
+            # If the place is dead set the row to `0` 
+            if row[i] == '0':
+                for j in range(len(row)):
+                    row[j] = '0'
+                # Add the place to the dead columns
+                dead_columns.append(i)
+            else:
+                # If the place is not dead, set the dead columns to `0`
+                for dead_column in dead_columns:
+                    row[i][dead_column] = '0'            
 
         return self.matrix_initial
 
@@ -199,44 +272,69 @@ class TFG:
         """ Token propagation:
             - propagate reachable/dead places,
             - learn new concurrent/independent places,
-            - memoize children.
+            - memoize successors.
         """
         # Initialization
-        children = []
+        successors = []
 
-        # If the node is a place from the initial net:
-        # - add the value (not '.') in the concurrency matrix (reachable / dead),
-        # - add to the children list.
+        # Case: partial relation and parents already propagated
+        if not self.complete_matrix and all(parent.propagated for parent in node.parents):
+            
+            # Update `propagated` flag of the node
+            node.propagated = True
+
+            # The products of the parents' predecessors are independent
+            # Can be treat as change_of_basis
+            for parent_1, parent_2 in itertools.combinations(node.parents, 2):
+                self.product(parent_1.predecessors, parent_2.predecessors, '0')
+
+            # Set the predecessors of the node
+            node.predecessors = [predecessor for parent in node.parents for predecessor in parent.predecessors]
+
+            # Update `dead` flag
+            if value == '0':
+                # If all parents are dead set the node to dead, otherwise cannot propagate a dead value anymore
+                if all(parent == '0' for parent in node.parents):
+                    node.dead = True
+                else:
+                    value = '.'
+
+        # Case: the node is a place from the initial net
         if not node.additional:
+            
+            # Set its value (if different from '.') in the concurrency matrix (non-dead / dead)
             if value != '.':
                 order = self.initial_net.order[node.id]
-                if self.matrix_initial[order][order] != '1':
-                    self.matrix_initial[order][order] = value
-            children.append(node)
+                self.matrix_initial[order][order] = value
+            
+            # Add the node to the successors and its predecessors list
+            successors.append(node)
+            node.predecessors.append(node)
+
+            # Set agglomerated nodes as independent
+            for agg_1, agg_2 in itertools.combinations(node.agglomerated, 2):
+                agg_1.independent.add(agg_2)
+                agg_2.independent.add(agg_1)
 
         # Token propagation over the agglomerated nodes
         for agglomerated in node.agglomerated:
-            new_children = self.token_propagation(agglomerated, value)
-            # Partial relation case
-            if not self.complete_matrix:
-                # Learn new independent places
-                self.product(new_children, children, '0')
-            children += new_children
-
-        # Token propagation over the redundant nodes
+            agg_successors = self.token_propagation(agglomerated, value)
+            successors += agg_successors
+            
+        # Token propagation over the redundancy nodes
         for redundant in node.redundant:
-            new_children = self.token_propagation(redundant, value)
-            # Learn new concurrent/independent places
-            if value != '.':
-                self.product(new_children, children, value)
-            children += new_children
+            red_successors = self.token_propagation(redundant, value)
+            # Learn new concurrent places
+            if value == '1':
+                self.product(red_successors, successors, value)
+            successors += red_successors
 
-        # Children memoization
+        # Successors memoization
         if memoize:
-            node.children = children
+            node.successors = successors
 
-        # Return children
-        return children
+        # Return successors
+        return successors
 
     def product(self, places1, places2, value):
         """ Set the cartesian product between two lists of places
@@ -245,9 +343,7 @@ class TFG:
         for place1, place2 in itertools.product(places1, places2):
             place1 = self.initial_net.order[place1.id]
             place2 = self.initial_net.order[place2.id]
-            # DAGs intersection management (cf. change_of_basis method)
-            if value == '1' or self.matrix_initial[max(place1, place2)][min(place1, place2)] == '.':
-                self.matrix_initial[max(place1, place2)][min(place1, place2)] = value
+            self.matrix_initial[max(place1, place2)][min(place1, place2)] = value
 
 
 class Node:
@@ -259,7 +355,8 @@ class Node:
     - a Boolean indicating if the node is an additional variable (not in the initial net),
     - a list of redundant nodes,
     - a list of agglomerated nodes,
-    - a list of children in the TFG (optional).
+    - a list of successors in the TFG (optional),
+    - a list of predecessors in the TFG (optional).
     """
 
     def __init__(self, id, additional=False):
@@ -267,10 +364,23 @@ class Node:
         """
         self.id = id
         self.additional = additional
+        
+        # Incoming arcs (parents)
+        self.parents = []
 
-        # Arcs
+        # Outgoing arcs (children)
         self.redundant = []
         self.agglomerated = []
 
-        # Descendants in the TFG that are not additional
-        self.children = []
+        # Propagated flag
+        self.propagated = False
+
+        # Dead flag
+        self.dead = False
+
+        # Succesors and predecessors in the TFG that are not additional
+        self.successors = []
+        self.predecessors = []
+
+        # Independent nodes set
+        self.independent = set()
